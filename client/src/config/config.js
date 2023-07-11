4f4754c4170cf2be55a00eac1dfe33793fe92505
KnotEnvy <jwsnyder@gmail.com>
2023-07-11 12:06:39 -0400

    backend url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://t-shirt-3js.onrender.com",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
